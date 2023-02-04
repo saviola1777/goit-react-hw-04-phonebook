f706029c37b755046b2e27b507c9f80681f9178c
Oleksandr Drahanchuk <draganchuk1991@ukr.net>
2023-02-04 18:10:57 +0200

    Зроби дз-3 таке яке має бути перед хуками

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -11,46 +11,50 @@ class App extends React.Component{
     contacts: [],
     filter:'',
     }
-    componentDidMount(){                                                   //рендерить розмітку з початкового стану тільки один раз тобто при загрузці
-      const contacts=  JSON.parse(localStorage.getItem('my-contacts')); //тут ми забрали наші номера шо ми добавляли раніше в localStorage 
-      if(contacts){                  //ТУТПЕРЕВІРЯЄМО localStorage(якшо в localStorage ще нічого немає тому умова приводиться до false бо там буде 0 тому  нам нічого не треба рендерити)
-        this.setState({contacts})           //ЯКШО УМОВА ПРИВОДИТЬСЯ ДО TRU ТОБТО В LOCALSTARAGE ЩОСЬ Є ТО МИ РЕНДЕРИМО 
+    componentDidMount(){                                                  
+      const contacts=  JSON.parse(localStorage.getItem('my-contacts')); 
+      if(contacts){                 
+        this.setState({contacts})
       } } 
 
-    componentDidUpdate(prevProps , prevState ){   //componentDidUpdate() ДОБАВЛЯЄМО В  localStorage ЗМІНИ НАЩОГО contacts-- ЦЕЙ МЕТОД ВИЗИВАЄТЬСЯ ПРИ ЗМІНІ STATE БАГАТО РАЗ 
+    componentDidUpdate(prevProps , prevState ){    
       const{contacts}=this.state ;
     if(prevState.contacts!==contacts){
       localStorage.setItem('my-contacts' , JSON.stringify(contacts))
-      console.log(`Попередній стан prevState.contacts.length :`,prevState.contacts.length)
-      console.log(`Теперішній стан contacts.length :`,contacts.length)
     }}
         
+    deleteContact = id => {
+      this.setState(prevState => {                                             
+     const removeContact=prevState.contacts.filter(contact=>contact.id!==id);  
+        return {contacts:removeContact};                    
+      });
+    };
 
-  checkNameInPhonebook = userName => {                
-    const { contacts } = this.state;                  ;
-    return contacts.some(({ name }) => name.toLowerCase() === userName.toLowerCase());  
-  };
+addContact = ({name , number})=>{
+this.setState(prevState => {
+  const{contacts}=prevState ;
+  if(this.isDublication(name)){
+    return alert(`${name} is already in contacts!`)
+  }
+ const newContact={
+  id:nanoid(3),
+  name, 
+  number,
+ }
+return{contacts:[newContact , ...contacts]}
+})}
 
-  addContact = (userName, userTel) => {                   
-    if (this.checkNameInPhonebook(userName)) {            
-    return alert(`${userName} is already in contacts!`)};  
-     this.setState(prevState => {          
-      return {
-        contacts: [                       
-          {                              
-            id: nanoid(4),                
-            name: userName,               
-            number: userTel,              
-          },
-          ...prevState.contacts,           
-        ],
-      };
-    });
-  };
+isDublication(name){
+const normalizeName= name.toLowerCase()
+const{contacts}=this.state
+const nameContact=contacts.find(({name})=>{
+  return(normalizeName===name.toLowerCase())
+})
+return Boolean(nameContact)
+}
 
    onHendleChange = e => {          
-    this.setState({ [e.target.name]: e.target.value });
-   console.log(e.target.name ,e.target.value)         
+    this.setState({ [e.target.name]: e.target.value });         
   };                                
  
 
@@ -63,22 +67,15 @@ class App extends React.Component{
     return result
 }
 
-deleteContact = id => {
-  this.setState(prevState => {                                             
- const removeContact=prevState.contacts.filter(contact=>contact.id!==id);  
-    return {contacts:removeContact};                    
-  });
-};
-
 render(){
- const {deleteContact, onHendleChange, addContact}=this
+ const {deleteContact, onHendleChange, addContact} = this ;
   const contacts=this.getFilteredContact();
-  //  console.log('Render')
+  console.log(this.state.contacts)
   return (
     
     <Cointeiner>
       <h2>Phonebook</h2>
-    <ContactForm  addContact={addContact}/>
+    <ContactForm  addContact={addContact} />
     <h2>Contacts</h2> 
     <Filter  onHendleChange={onHendleChange} />
     <ContactList contacts={contacts} deleteContact={deleteContact}/>
@@ -88,8 +85,3 @@ render(){
   }
   export default App
   
- //ЖИТЄВИЙ ЦИКЛ ВИКЛИКАЄТЬСЯ КОНСТРУКТОР , ПОТІМ РЕНДЕР ПОТІМ РЕАКТ ОБНОВЛЯЄ DOM І ПОТІМ ВИКЛИКАЄТЬСЯ ПЕРШИЙ МЕТОД componentDidMount ВІН ВИКЛИКАЄТЬСЯ ПІСЛЯ ТОГО ЯК ВСЕ ВІДМАЛЮЄТЬСЯ
- //І ЛИШЕ ОДИН РАЗ А ВЖЕ ПІСЛЯ ЗМІНИ SETSTATE ТОБТО КОЛИ МИ ДОБАВИЛИ ЯКИЙСЬ КОНТАКТ ЧИ ВИДАЛИТИ ВОНО ВИКОНУЄ ЗНОВУ РЕНДЕРИТЬ ОНОВЛЮЄ DOM І ТОДІ СПРАЦЮВУЄ componentDidUpdate
- //componentDidMount ====ВІДМАЛЮУЮТЬСЯ КОМПОНЕНТИ В ПЕРШИЙ РАЗ ТОБТО ЯКЩО СПОЧАТКУ STATE.CONTACTS[] ТО ВІДМАЛЮЄТЬСЯ ПУСТИЙ МАСИВ
- //  componentDidUpdate
-// Объекты веб-хранилища localStorage дозволяє зберігати  пари ключ/значеня в браузере.
--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,49 +1,67 @@
-  import PropTypes from "prop-types";
+import PropTypes from "prop-types";
 import css from "components/ContactForm/ContactForm.module.css"
+import React from 'react';
+// import inititalState from 'components/ContactForm/inititalState'   тут ми можемо зберігати наші обєкти такі як name , number
 
-const ContactForm = ({ addContact }) => {
-  const onFormSabmit = e => {
-    e.preventDefault();
-    const userName = e.currentTarget.name.value;
-    const userTel = e.currentTarget.number.value;
-    addContact(userName, userTel);
-    e.currentTarget.reset();
-    
-  }; 
+class ContactForm extends React.Component {
+  state = {
+    name: '',                            //могли б просто розпилити наш inititalState малоб бути  state={...inititalState} а далі коли міняється стейт міняються і дані в ньому
+    number: '',
+  } 
+  onHendleSubmit = (e) => {             
+    e.preventDefault();                           //при сабміті не перезагружає
+    const { addContact } = this.props;            //передаємо мій  пропс
+    addContact({ ...this.state })                //в мій проп ми розпилюємо тещо знаходиться в цьому стейті name , number 
+    this.setState({ name: '', number: '', })     //this.setState({ ...inititalState } можна було б так тому що він бере копію.inititalState в в ньому name: '', number: '',
 
-return( 
-  
-    <div className={css.wrapper}>
-    <form onSubmit={e => onFormSabmit(e)} className={css.form}>
-    <label className={css.name}> Name
-        <input
-  className={css.input}      
-  type="text"
-  name="name"
-  pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-  title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-  required
-  
-/> </label>
-     <label className={css.name}>Number
-     <input
-          className={css.input}
-					type="tel"
-					name="number"
-					pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-					title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
-					required
-					/>
-     </label>
-     <button className={css.button} type="submit">Add contact</button>
+  }
+  onHendleChange = ({ target }) => {
+    const { name, value } = target
+    this.setState({ [name]: value });
 
-      </form>
+  };
+  render() {
+    const { name, number } = this.state;
+    const { onHendleSubmit, onHendleChange } = this
+    console.log(this.state)
+    return (
+
+      <div className={css.wrapper}>
+        <form onSubmit={onHendleSubmit} className={css.form}>
+          <label className={css.name}> Name
+            <input
+              className={css.input}
+              type="text"
+              name="name"
+              pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+              title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+              required
+              value={name}
+              onChange={onHendleChange}
+
+            /> </label>
+          <label className={css.name}>Number
+            <input
+              className={css.input}
+              type="tel"
+              name="number"
+              pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+              title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+              required
+              value={number}
+              onChange={onHendleChange}
+            />
+          </label>
+          <button className={css.button} type="submit">Add contact</button>
+
+        </form>
       </div>
-       ) 
- }
- ContactForm.prototype={
-  addContact:PropTypes.func.isRequired,
- }
+    )
+  }
+}
+ContactForm.Prototype = {
+  onSubmit: PropTypes.func.isRequired,
+}
 
 export default ContactForm;
 
